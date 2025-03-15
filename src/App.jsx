e15442e93448a68f943a282d1f70d671d3024799
abolfazl-f11 <abolfazlfiroozan53@gmail.com>
2025-03-15 20:36:56 +0330

    add signupPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import LoginPage from './Authentication/Login/LoginPage.jsx';
+import SignUpPage from './Authentication/SignUp/SignUpPage.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
 
   return (
     <div>
-      <LoginPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signUp' element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 
--- src/Authentication/SignUp/SignUpPage.jsx
+++ src/Authentication/SignUp/SignUpPage.jsx
@@ -2,25 +2,30 @@ import { useState } from "react";
 import { IconButton } from "@mui/material";
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import PersonIcon from '@mui/icons-material/Person';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
 
 const LoginPage = () => {
     const [showPassword, setShowPassword] = useState(false); /* نشون دادن پسورد */
     const [userEmail, setUserEmail] = useState("");/* ذخیره ایمیل کاربر */
+    const [userName, setUserName] = useState("");/* ذخیره نام کاربر */
     const [userPassword, setUserPassword] = useState("") /* ذخیره رمز کاربر */
     const navigate = useNavigate();
 
     const handleLogin = async () => {/* اطلاعات کاربر ارسال به بکند */
         const data = {
+            userName: userName,
             userEmail: userEmail,
-            password: userPassword,
+            password: userPassword
         };
 
         try {
-            const response = await axios.post('http://192.168.137.1:3000/auth/login', data);
+            const response = await axios.post('http://192.168.137.1:3000/auth/register', data);
             console.log('Login successful:', response.data);
-            navigate('/')
+            navigate("/login");
 
         } catch (error) {
             console.error('error sending data:', error)
@@ -28,6 +33,9 @@ const LoginPage = () => {
     }
 
 
+    const setCookieRemmber = () => {
+        Cookies.set('remmber',)
+    }
 
 
     return (
@@ -43,7 +51,7 @@ const LoginPage = () => {
                     <div className="w-[50%] h-full flex justify-center items-center">
                         <div className="w-[80%] h-full bg-white rounded-xl p-6 justify-self-center">
                             {/* قسمت header */}
-                            <div className="h-[30%] flex flex-col justify-between">
+                            <div className="h-[25%] flex flex-col justify-between">
                                 <div className="flex flex-col justify-center items-center">
                                     <h2 className="text-[#2F2F2F] font-bold text-md">Welcome to</h2>
                                     <h1 className="text-[#6358DC] font-bold text-4xl">Taskora</h1>
@@ -51,7 +59,7 @@ const LoginPage = () => {
                                 <div className="flex flex-col gap-4">
                                     <button className="h-14 rounded-lg shadow shadow-[#d3d3d3] flex items-center justify-center gap-4 cursor-pointer">
                                         <img className="w-6 h-6" src="src/Authentication/Login/img/search 1.svg" alt="" />
-                                        Login with Google
+                                        Sign up with Google
                                     </button>
                                 </div>
                             </div>
@@ -59,6 +67,13 @@ const LoginPage = () => {
                             {/* قسمت inputs */}
                             <div className="w-full h-[70%] flex flex-col gap-6">
                                 <div className="flex flex-col gap-4 mt-4">
+                                    <div className="w-full h-[58px] bg-[#ECECEC] flex items-center justify-center gap-6 rounded-md px-4">
+                                        <PersonIcon fontSize="large" />
+                                        <div className="flex flex-col w-full h-full py-2">
+                                            <label className="text-[12px]">Name</label>
+                                            <input name="text" value={userName} onChange={(e) => setUserName(e.target.value)} type="email" className="outline-none bg-transparent" placeholder="abolfazl" />
+                                        </div>
+                                    </div>
                                     <div className="w-full h-[58px] bg-[#ECECEC] flex items-center justify-center gap-6 rounded-md px-6">
                                         <img className="w-6 h-6" src="src/Authentication/Login/img/iconLogin1.svg" alt="" />
                                         <div className="flex flex-col w-full h-full py-2">
@@ -66,7 +81,7 @@ const LoginPage = () => {
                                             <input name="email" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} type="email" className="outline-none bg-transparent" placeholder="example@gmail.com" />
                                         </div>
                                     </div>
-                                    <div className="w-full h-[58px] bg-[#ECECEC] flex items-center justify-center gap-6 rounded-md px-6">
+                                    <div className="w-full h-[58px] bg-[#ECECEC] flex items-center justify-center gap-6 rounded-md pl-6 pr-3">
                                         <img className="w-6 h-6" src="src/Authentication/Login/img/iconLogin2.svg" alt="" />
                                         <div className="flex flex-col w-full h-full py-2">
                                             <label className="text-[12px]">Password</label>
@@ -77,17 +92,10 @@ const LoginPage = () => {
                                         </IconButton>
                                     </div>
                                 </div>
-                                <div className="flex flex-col gap-6">
-                                    <div className="flex justify-between">
-                                        <div className="flex gap-2 items-center">
-                                            <input name="remember" type="checkbox" className="cursor-pointer" />
-                                            <p className="text-[13px]">Remember me</p>
-                                        </div>
-                                        <a href="#" className="text-[13px] text-[#6358DC]">Forgot Password?</a>
-                                    </div>
-                                    <div className="flex flex-col gap-8 items-center">
-                                        <button type="button" className="w-full h-[60px] bg-[#6358DC] text-white rounded-lg cursor-pointer" onClick={handleLogin}>Login</button>
-                                        <p>Don't have an account?<a href="/signUp" className="text-[#6358DC]">Sign Up</a></p>
+                                <div className="flex flex-col gap-8">
+                                    <div className="flex flex-col gap-6 items-center">
+                                        <button type="button" className="w-full h-[60px] bg-[#6358DC] text-white rounded-lg cursor-pointer" onClick={handleLogin}>Create Account</button>
+                                        <p>Don't have an account? <a href="/login" className="text-[#6358DC]">Login</a></p>
                                     </div>
                                 </div>
                             </div>

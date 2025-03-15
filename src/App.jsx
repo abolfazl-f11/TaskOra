73e74983761bb09f79ca31e4900c89ff6e05256f
abolfazl-f11 <abolfazlfiroozan53@gmail.com>
2025-03-15 16:14:06 +0330

    loginpage add api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css';
-import LoginPage from './Login/LoginPage';
+import LoginPage from './Authentication/Login/LoginPage.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
 
